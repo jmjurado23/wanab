2b4a3404d5aac530fce0603b9a3a01b9b08eb338
Juanma <jmj23elviso@gmail.com>
2014-01-23 17:36:54 +0100

    Initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wanab::Application.config.secret_key_base = '22a533b5aa708f52da9fdc195688144c33116891fb2881ea2c73c1db40a923ec85a6ed5b5543f6554ed8fd2ae517e1a298e7abd0eac9c381a7e6a091945e1ff3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wanab::Application.config.session_store :cookie_store, key: '_wanab_session'
